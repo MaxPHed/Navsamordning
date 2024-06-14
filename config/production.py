76fa17f0542ed59e923e14afd75ee6ba0135cb1f
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2024-06-14 09:24:19 +0200

    new production config

--- config/production.py
+++ config/production.py
@@ -3,19 +3,19 @@ import os
 
 class Config:
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
 
-
-# Configure Postgres database based on connection string of the libpq Keyword/Value form
-# https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
-DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
-)
+    # Configure Postgres database based on connection string of the libpq Keyword/Value form
+    # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+    conn_str = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
+    if conn_str:
+        conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+            dbuser=conn_str_params['user'],
+            dbpass=conn_str_params['password'],
+            dbhost=conn_str_params['host'],
+            dbname=conn_str_params['dbname']
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
